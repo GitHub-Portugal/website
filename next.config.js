4bfacf1ed25f4c14094bb8f287ff0f2fabea1a61
Miguel Rocha <miguelrocha.dev@gmail.com>
2023-09-16 18:08:41 +0100

    :wrench: chore: add support for mdx files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+const withMDX = require("@next/mdx")();
+
+module.exports = withMDX(nextConfig);
