f477bd4f5b153b5a2bb004573012ec426b5d6a3d
Miguel Rocha <miguelrocha.dev@gmail.com>
2023-09-16 18:47:22 +0100

    :lipstick: feat: add custom styles to html tags

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,22 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h2: ({ children }) => (
+      <h2 className="border-b border-b-light-gray-2 py-2 text-xl font-extrabold first:pt-0 sm:text-2xl md:text-3xl">
+        {children}
+      </h2>
+    ),
+    a: ({ children, href }) => (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="font-bold hover:underline focus:underline"
+      >
+        {children}
+      </a>
+    ),
+    p: ({ children }) => <p className="text-base sm:text-lg">{children}</p>,
     ...components,
   };
 }
